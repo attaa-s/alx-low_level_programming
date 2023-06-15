f19514ee89125258eebc1a17d85b17ba302f99d7
attaa-s <attaa.sobhi@gmail.com>
2023-06-15 00:48:37 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main(void)
-/** this fuction return 0
- * main - prints comments 
- */
+/* this fuction return  main - prints comments */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
