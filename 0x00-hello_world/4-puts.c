17e46f7dd25f0974e951ce5436a4c7b349153b0e
attaa-s <attaa.sobhi@gmail.com>
2023-06-15 01:12:34 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - start
- *
- *return - Always 0 (Success)
- */
-
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
